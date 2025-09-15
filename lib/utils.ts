e55f33867e2a1063874603c437cfc88a8a39d06d
unknown <chaturvedivimedha@gmail.com>
2025-09-16 01:58:41 +0530

    summary of expenses & income

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { eachDayOfInterval, isSameDay } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -19,3 +20,42 @@ export function formatCurrency(value: number) {
     minimumFractionDigits: 2,
   }).format(finalValue);
 };
+export function calculatePercentageChange(
+  current: number,
+  previous: number,
+){
+  if(previous===0){
+    return previous===current?0:100;
+  }
+  return ((current-previous)/previous)*100;
+}
+export function fillMissingDays(
+  activeDays:{
+    date:Date,
+    income: number;
+    expenses:number;
+  }[],
+  startDate:Date,
+  endDate:Date,
+){
+  if(activeDays.length===0){
+    return[];
+  }
+  const allDays=eachDayOfInterval({
+    start:startDate,
+    end:endDate,
+  });
+  const transactionsByDay=allDays.map((day)=>{
+    const found=activeDays.find((d)=>isSameDay(d.date,day));
+    if(found){
+      return found;
+    }else{
+      return{
+        date:day,
+        income:0,
+        expenses:0,
+      };
+    }
+  });
+  return transactionsByDay;
+}
