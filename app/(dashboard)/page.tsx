3d31dee607e894fc8ef83b8600658d5736c24db7
unknown <chaturvedivimedha@gmail.com>
2025-09-18 04:03:13 +0530

    fixing bugs 2

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,12 +1,20 @@
+import { Suspense } from "react";
 import { DataCharts } from "@/components/data-charts";
 import { DataGrid } from "@/components/data-grid";
 
-export default function DashboardPage(){
+const DashboardPageContent = () => {
   return(
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <DataGrid/>
       <DataCharts/>
-
     </div>
+  );
+};
+
+export default function DashboardPage(){
+  return(
+    <Suspense fallback={<div>Loading...</div>}>
+      <DashboardPageContent />
+    </Suspense>
   );
 };
